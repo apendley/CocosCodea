e097c0f1e2792ec0c71bb0dd5c94f7e2e86cda41
Aaron Pendley <aaronpendley@gmail.com>
2012-09-27 23:08:41 -0400

    - cleaned up todo list

--- CocosCodea.codea/todo.lua
+++ CocosCodea.codea/todo.lua
@@ -1,26 +1,17 @@
--- * color should not need special patching now; update it
--- * play with http.request
 -- * implement a physics sprite
--- * re-implement actions so that all actions support callbacks and repeating/looping/easing
 -- * CCActionCatmullRom
 -- * implement remaining transitions
 -- * add prioritized updates to CCScheduler and CCNode classes
 -- * add non-targeted touch handling
+-- * implement tile map node(s)
 -- * implement bitmapped fonts
--- * implement tilemaps (using mesh)
 -- * implement bitmapped font labels
--- * lazily create children table(?)
--- * do fast children sort in visit(?)
 -- * implement skew
 -- * CCEaseElastic(in/out/inout), CCBounce(in/out/inout), CCEaseBack(in/out/inout)
 -- * skewBy, skewTo, BezierBy, BezierTo
 -- * CCReverseTime, CCAnimate, CCTargetedAction
 -- * implement particle systems
--- * implement tile map node
 -- * CCRenderTexture
-
--- * write alternative version of sprite() that takes (s,t,u,v)
--- * in backend, and try to get TLL to use it
 -- * when codea allows (s, t, u, v) to be specfied in sprite()
     -- * add textureRect to CCSprite
     
